8209ac237882ea58ab2288c2c711614cd0905099
sangdi <1592807515@qq.com>
2022-05-03 11:05:59 +0800

    rth

---
+++ render props模式/src/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './index.css'
+/* 
+  Context
+*/
+
+
+
+class App extends React.Component {
+  render() {
+    return (
+     <div>
+       <h1>我是一个组件</h1>
+       <Chidrenn>{(mouse)=>{
+         return <p>{mouse.x}  {mouse.y}</p>
+       }}</Chidrenn>
+     </div>
+    )
+  }
+}
+
+class Chidrenn extends React.Component {
+  state={
+    x:0,
+    y:0
+  }
+  move=(e)=>{
+    this.setState({
+      x:e.clientX,
+      y:e.clientY
+    })
+  }
+  componentDidMount(){
+    window.addEventListener('mousemove',this.move)
+  }
+  render() {
+    return this.props.children(this.state)
+  }
+}
+
+
+
+
+ReactDOM.render(<App/>, document.getElementById('root'))
